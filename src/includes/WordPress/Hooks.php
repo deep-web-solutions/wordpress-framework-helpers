048366156288a3043a06f50b91ca0b8be8077591
Antonius Hegyes <a.hegyes@deep-web-solutions.de>
2021-02-20 01:24:34 +0100

    added a useful hooks helper

--- src/includes/WordPress/Hooks.php
+++ src/includes/WordPress/Hooks.php
@@ -49,4 +49,22 @@ final class Hooks {
 	public static function get_current_hook_priority(): int {
 		return $GLOBALS['wp_filter'][ current_filter() ]->current_priority();
 	}
+
+	/**
+	 * Enqueues a callable to run on the current hook's next priority, basically simulating a "tick".
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @param   callable    $func           Function to enqueue.
+	 * @param   int         $accepted_args  The number of arguments the function accepts. Default 1.
+	 *
+	 * @return  bool
+	 */
+	public static function enqueue_on_next_tick( callable $func, int $accepted_args = 1 ): bool {
+		$current_priority = self::get_current_hook_priority();
+		return ( PHP_INT_MAX === $current_priority )
+			? false
+			: add_action( current_action(), $func, $current_priority + 1, $accepted_args );
+	}
 }
