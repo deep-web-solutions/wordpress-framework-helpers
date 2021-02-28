b9b606cc974aad7bc17a46326a00bc2761d02c3b
Antonius Hegyes <a.hegyes@deep-web-solutions.de>
2021-02-28 23:54:38 +0100

    Left room for future modules

--- bootstrap.php
+++ bootstrap.php
@@ -94,13 +94,20 @@ function dws_wp_framework_get_helpers_min_wp() {
 // Bootstrap the helpers (maybe)!
 if ( dws_wp_framework_check_php_wp_requirements_met( dws_wp_framework_get_helpers_min_php(), dws_wp_framework_get_helpers_min_wp() ) ) {
 	$dws_helpers_init_function = function() {
-		define( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_HELPERS_INIT', defined( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_BOOTSTRAPPER_INIT' ) && DWS_WP_FRAMEWORK_BOOTSTRAPPER_INIT );
+		define(
+			__NAMESPACE__ . '\DWS_WP_FRAMEWORK_HELPERS_INIT',
+			apply_filters(
+				'dws_wp_framework_helpers_init_status',
+				defined( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_BOOTSTRAPPER_INIT' ) && DWS_WP_FRAMEWORK_BOOTSTRAPPER_INIT,
+				__NAMESPACE__
+			)
+		);
 	};
 
 	if ( did_action( 'plugins_loaded' ) ) {
 		call_user_func( $dws_helpers_init_function );
 	} else {
-		add_action( 'plugins_loaded', $dws_helpers_init_function, PHP_INT_MIN );
+		add_action( 'plugins_loaded', $dws_helpers_init_function, PHP_INT_MIN + 100 );
 	}
 } else {
 	define( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_HELPERS_INIT', false );
