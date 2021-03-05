25163e2ac4973a57aecfdd0715d31f6856ace3b1
Antonius Hegyes <a.hegyes@deep-web-solutions.de>
2021-03-05 14:01:23 +0100

    Added new object helpers

--- src/includes/DataTypes/Objects.php
+++ src/includes/DataTypes/Objects.php
@@ -97,4 +97,40 @@ final class Objects {
 
 		return $traits;
 	}
+
+	/**
+	 * Returns whether a given trait is used in the inheritance tree of a given class or another trait.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @SuppressWarnings(PHPMD.BooleanArgumentFlag)
+	 *
+	 * @param   string              $trait          Trait to search for.
+	 * @param   object|string       $class          An object (class instance) or a string (class name) to investigate.
+	 * @param   bool                $autoload       Whether to allow this function to load the class automatically through the __autoload() magic method.
+	 *
+	 * @return  bool
+	 */
+	public static function has_trait_deep( string $trait, $class, bool $autoload = true ): bool {
+		return in_array( $trait, self::class_uses_deep_list( $class, $autoload ), true );
+	}
+
+	/**
+	 * Returns whether a given trait is used in the given class.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @SuppressWarnings(PHPMD.BooleanArgumentFlag)
+	 *
+	 * @param   string              $trait          Trait to search for.
+	 * @param   object|string       $class          An object (class instance) or a string (class name) to investigate.
+	 * @param   bool                $autoload       Whether to allow this function to load the class automatically through the __autoload() magic method.
+	 *
+	 * @return  bool
+	 */
+	public static function has_trait( string $trait, $class, bool $autoload = true ): bool {
+		return in_array( $trait, class_uses( $class, $autoload ), true );
+	}
 }
