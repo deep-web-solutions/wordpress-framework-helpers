bc4fa89b2477b0c90a1c1c60e8cf0494a17c3a91
Antonius Hegyes <a.hegyes@deep-web-solutions.de>
2021-03-06 21:15:31 +0100

    Removed a superfluous array helper

--- src/includes/DataTypes/Arrays.php
+++ src/includes/DataTypes/Arrays.php
@@ -74,32 +74,4 @@ final class Arrays {
 	public static function search_keys( array $array, $needle, bool $strict = false, callable $callback = null ): ?array {
 		return self::search_values( array_keys( $array ), $needle, $strict, $callback );
 	}
-
-	/**
-	 * Removes an element from an array by its value. If the value exists more than once, removes all of them.
-	 *
-	 * @since   1.0.0
-	 * @version 1.0.0
-	 *
-	 * @SuppressWarnings(PHPMD.BooleanArgumentFlag)
-	 *
-	 * @param   array           $array      The original array passed by reference.
-	 * @param   mixed           $needle     The value that should be unset.
-	 * @param   bool            $strict     Whether to perform type checks or not.
-	 * @param   callable|null   $callback   Optional callback to run the value through before needle comparison.
-	 *
-	 * @return  array|null  The removed elements, or null if nothing removed.
-	 */
-	public static function unset_elements_by_value( array &$array, $needle, bool $strict = true, ?callable $callback = null ): ?array {
-		$to_unset = self::search_values( $array, $needle, $strict, $callback );
-		if ( is_null( $to_unset ) ) {
-			return null;
-		}
-
-		foreach ( array_keys( $to_unset ) as $key ) {
-			unset( $array[ $key ] );
-		}
-
-		return $to_unset;
-	}
 }
