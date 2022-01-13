8454a2d86b762dec4bc13504b00b121200647b84
Antonius Hegyes <a.hegyes@deep-web-solutions.com>
2022-01-13 19:32:06 +0100

    serious refactoring of the Objects helpers

--- src/includes/DataTypes/Objects.php
+++ src/includes/DataTypes/Objects.php
@@ -8,7 +8,7 @@ namespace DeepWebSolutions\Framework\Helpers\DataTypes;
  * A collection of very useful misc helpers to be used throughout the projects.
  *
  * @since   1.0.0
- * @version 1.4.0
+ * @version 1.7.0
  * @author  Antonius Hegyes <a.hegyes@deep-web-solutions.com>
  * @package DeepWebSolutions\WP-Framework\Helpers\DataTypes
  */
@@ -29,71 +29,79 @@ final class Objects {
 	}
 
 	/**
-	 * Gets the description of the trait inheritance scheme of a given trait.
+	 * Returns all the traits used by a trait and its traits as a hierarchical array.
 	 *
 	 * @since   1.2.0
-	 * @version 1.2.0
+	 * @version 1.7.0
 	 *
 	 * @SuppressWarnings(PHPMD.UndefinedVariable)
 	 *
 	 * @param   string  $trait          The trait to act upon.
 	 * @param   bool    $autoload       Whether to allow this function to load the class automatically through the __autoload() magic method.
 	 *
-	 * @return  array
+	 * @return  array[]|null
 	 */
-	public static function trait_uses_deep( string $trait, bool $autoload = true ): array {
+	public static function trait_uses_deep( string $trait, bool $autoload = true ): ?array {
 		static $results = array();
 
 		if ( isset( $results[ $trait ] ) ) {
 			$traits = $results[ $trait ];
 		} else {
-			$traits      = array();
-			$used_traits = \class_uses( $trait, $autoload ) ?: array();
+			$traits      = null;
+			$used_traits = Arrays::validate( \class_uses( $trait, $autoload ), null );
 
-			foreach ( $used_traits as $used_trait ) {
-				$traits[ $used_trait ] = self::trait_uses_deep( $used_trait );
-			}
+			if ( false === \is_null( $used_traits ) ) {
+				$traits = array();
 
-			if ( true === $autoload ) {
-				$results[ $trait ] = $traits;
-			}
-		}
+				foreach ( $used_traits as $used_trait ) {
+					$traits[ $used_trait ] = self::trait_uses_deep( $used_trait, $autoload );
+				}
 
-		return $traits;
-	}
-
-	/**
-	 * Gets a list of all the traits used by a given trait and its own traits.
-	 *
-	 * @since   1.2.0
-	 * @version 1.2.0
-	 *
-	 * @SuppressWarnings(PHPMD.UndefinedVariable)
-	 *
-	 * @param   string  $trait          The trait to act upon.
-	 * @param   bool    $autoload       Whether to allow this function to load the class automatically through the __autoload() magic method.
-	 *
-	 * @return  string[]
-	 */
-	public static function trait_uses_deep_list( string $trait, bool $autoload = true ): array {
-		static $results = array();
-
-		if ( isset( $results[ $trait ] ) ) {
-			$traits = $results[ $trait ];
-		} else {
-			$traits      = array();
-			$used_traits = self::trait_uses_deep( $trait, $autoload );
-
-			foreach ( $used_traits as $used_trait => $children_traits ) {
-				$traits[ $used_trait ] = $used_trait;
-				foreach ( \array_keys( $children_traits ) as $child_trait ) {
-					$traits[ $child_trait ] = $child_trait;
-					$traits                += self::trait_uses_deep_list( $child_trait );
+				if ( true === $autoload ) {
+					$results[ $trait ] = $traits;
 				}
 			}
+		}
 
-			if ( true === $autoload ) {
-				$results[ $trait ] = $traits;
+		return $traits;
+	}
+
+	/**
+	 * Returns all the traits used by a trait and its traits as a list array.
+	 *
+	 * @since   1.2.0
+	 * @version 1.7.0
+	 *
+	 * @SuppressWarnings(PHPMD.UndefinedVariable)
+	 *
+	 * @param   string  $trait          The trait to act upon.
+	 * @param   bool    $autoload       Whether to allow this function to load the class automatically through the __autoload() magic method.
+	 *
+	 * @return  string[]|null
+	 */
+	public static function trait_uses_deep_list( string $trait, bool $autoload = true ): ?array {
+		static $results = array();
+
+		if ( isset( $results[ $trait ] ) ) {
+			$traits = $results[ $trait ];
+		} else {
+			$traits      = null;
+			$used_traits = self::trait_uses_deep( $trait, $autoload );
+
+			if ( false === \is_null( $used_traits ) ) {
+				$traits = array();
+
+				foreach ( $used_traits as $used_trait => $children_traits ) {
+					$traits[ $used_trait ] = $used_trait;
+					foreach ( \array_keys( $children_traits ) as $child_trait ) {
+						$traits[ $child_trait ] = $child_trait;
+						$traits                += self::trait_uses_deep_list( $child_trait, $autoload );
+					}
+				}
+
+				if ( true === $autoload ) {
+					$results[ $trait ] = $traits;
+				}
 			}
 		}
 
@@ -101,12 +109,11 @@ final class Objects {
 	}
 
 	/**
-	 * Gets the description of the top-down used traits and their inheritance scheme of a given class.
+	 * Returns all traits used by a class, its parent classes and trait of their traits as a hierarchical array.
 	 *
 	 * @since   1.0.0
-	 * @version 1.2.0
+	 * @version 1.7.0
 	 *
-	 * @SuppressWarnings(PHPMD.CyclomaticComplexity)
 	 * @SuppressWarnings(PHPMD.UndefinedVariable)
 	 *
 	 * @see     https://www.php.net/manual/en/function.class-uses.php#122427
@@ -114,31 +121,29 @@ final class Objects {
 	 * @param   object|string   $class          An object (class instance) or a string (class name).
 	 * @param   bool            $autoload       Whether to allow this function to load the class automatically through the __autoload() magic method.
 	 *
-	 * @return  array
+	 * @return  array[]|null
 	 */
-	public static function class_uses_deep( $class, bool $autoload = true ): array {
+	public static function class_uses_deep( $class, bool $autoload = true ): ?array {
 		static $results = array();
 
-		$class = \is_object( $class ) ? \get_class( $class ) : $class;
+		$class = \is_object( $class ) ? \get_class( $class ) : \strval( $class );
 
 		if ( isset( $results[ $class ] ) ) {
 			$traits = $results[ $class ];
 		} else {
-			$traits = array();
+			$traits        = null;
+			$class_parents = Arrays::validate( \class_parents( $class ), null );
 
-			foreach ( \array_reverse( \class_parents( $class ) ) + array( $class => $class ) as $current_class ) {
-				$traits[ $current_class ] = \class_uses( $current_class, $autoload );
-			}
+			if ( false === \is_null( $class_parents ) ) {
+				$traits = array();
 
-			foreach ( $traits as $current_class => $traits_list ) {
-				foreach ( $traits_list as $current_trait ) {
-					$traits[ $current_class ][ $current_trait ] = self::trait_uses_deep( $current_trait );
+				foreach ( \array_reverse( $class_parents ) + array( $class => $class ) as $current_class ) {
+					$traits[ $current_class ] = self::trait_uses_deep( $current_class, $autoload );
 				}
-			}
 
-			// Cache the result if autoload is active.
-			if ( true === $autoload ) {
-				$results[ $class ] = $traits;
+				if ( true === $autoload ) {
+					$results[ $class ] = $traits;
+				}
 			}
 		}
 
@@ -146,39 +151,42 @@ final class Objects {
 	}
 
 	/**
-	 * Gets a list of all the traits used by a class, its parent classes and all of their own traits
+	 * Returns all traits used by a class, its parent classes and trait of their traits as a list array.
 	 *
 	 * @since   1.0.0
-	 * @version 1.2.0
+	 * @version 1.7.0
 	 *
 	 * @SuppressWarnings(PHPMD.UndefinedVariable)
 	 *
 	 * @param   object|string   $class          An object (class instance) or a string (class name).
 	 * @param   bool            $autoload       Whether to allow this function to load the class automatically through the __autoload() magic method.
 	 *
-	 * @return  array
+	 * @return  string[]|null
 	 */
-	public static function class_uses_deep_list( $class, bool $autoload = true ): array {
+	public static function class_uses_deep_list( $class, bool $autoload = true ): ?array {
 		static $results = array();
 
-		$class = \is_object( $class ) ? \get_class( $class ) : $class;
+		$class = \is_object( $class ) ? \get_class( $class ) : \strval( $class );
 
 		if ( isset( $results[ $class ] ) ) {
 			$traits = $results[ $class ];
 		} else {
-			$traits      = array();
+			$traits      = null;
 			$used_traits = self::class_uses_deep( $class, $autoload );
 
-			foreach ( $used_traits as $traits_list ) {
-				foreach ( \array_keys( $traits_list ) as $used_trait ) {
-					$traits[ $used_trait ] = $used_trait;
-					$traits               += self::trait_uses_deep_list( $used_trait );
-				}
-			}
+			if ( false === \is_null( $used_traits ) ) {
+				$traits = array();
 
-			// Cache the result if autoload is active.
-			if ( true === $autoload ) {
-				$results[ $class ] = $traits;
+				foreach ( $used_traits as $traits_list ) {
+					foreach ( \array_keys( $traits_list ) as $used_trait ) {
+						$traits[ $used_trait ] = $used_trait;
+						$traits               += self::trait_uses_deep_list( $used_trait, $autoload );
+					}
+				}
+
+				if ( true === $autoload ) {
+					$results[ $class ] = $traits;
+				}
 			}
 		}
 
@@ -186,35 +194,41 @@ final class Objects {
 	}
 
 	/**
-	 * Returns whether a given trait is used in the given class.
+	 * Returns whether a given trait is used in a given class/trait.
 	 *
 	 * @since   1.0.0
-	 * @version 1.2.0
+	 * @version 1.7.0
 	 *
-	 * @param   string              $trait          Trait to search for.
-	 * @param   object|string       $class          An object (class instance) or a string (class name) to investigate.
-	 * @param   bool                $autoload       Whether to allow this function to load the class automatically through the __autoload() magic method.
+	 * @param   string          $trait              Trait to search for.
+	 * @param   object|string   $class_or_trait     An object (class instance) or a string (class or trait name) to investigate.
+	 * @param   bool            $autoload           Whether to allow this function to load the class automatically through the __autoload() magic method.
 	 *
-	 * @return  bool
+	 * @return  bool|null
 	 */
-	public static function has_trait( string $trait, $class, bool $autoload = true ): bool {
-		$traits = \class_uses( $class, $autoload ) ?: array();
-		return isset( $traits[ $trait ] );
+	public static function has_trait( string $trait, $class_or_trait, bool $autoload = true ): ?bool {
+		$traits = Arrays::validate( \class_uses( $class_or_trait, $autoload ), null );
+		return \is_null( $traits ) ? null : isset( $traits[ $trait ] );
 	}
 
 	/**
-	 * Returns whether a given trait is used in the inheritance tree of a given class or another trait.
+	 * Returns whether a given trait is used in the inheritance tree of a given class/trait.
 	 *
 	 * @since   1.0.0
-	 * @version 1.0.0
+	 * @version 1.7.0
 	 *
-	 * @param   string              $trait          Trait to search for.
-	 * @param   object|string       $class          An object (class instance) or a string (class name) to investigate.
-	 * @param   bool                $autoload       Whether to allow this function to load the class automatically through the __autoload() magic method.
+	 * @param   string          $trait          Trait to search for.
+	 * @param   object|string   $class_or_trait An object (class instance) or a string (class or trait name) to investigate.
+	 * @param   bool            $autoload       Whether to allow this function to load the class automatically through the __autoload() magic method.
 	 *
-	 * @return  bool
+	 * @return  bool|null
 	 */
-	public static function has_trait_deep( string $trait, $class, bool $autoload = true ): bool {
-		return isset( self::class_uses_deep_list( $class, $autoload )[ $trait ] );
+	public static function has_trait_deep( string $trait, $class_or_trait, bool $autoload = true ): ?bool {
+		if ( true === \trait_exists( $class_or_trait, $autoload ) ) {
+			return isset( self::trait_uses_deep_list( $class_or_trait, $autoload )[ $trait ] );
+		} elseif ( true === \class_exists( $class_or_trait, $autoload ) ) {
+			return isset( self::class_uses_deep_list( $class_or_trait, $autoload )[ $trait ] );
+		}
+
+		return null;
 	}
 }
